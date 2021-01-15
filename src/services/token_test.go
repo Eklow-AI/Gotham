d91f8a9865cda680c03c71b001a2f499077f005c
Daniel Lobaton <dlobaton@eklow.ai>
2021-01-14 21:34:41 -0500

    added some unit tests

--- src/services/token_test.go
+++ src/services/token_test.go
@@ -7,6 +7,6 @@ import (
 func TestUtypeRankings(t *testing.T) {
 	token := CreateToken("biggie@smallz.com")
 	if len(token) < 35 {
-		t.Errorf("Token length is to short, should be greater than 35 characters")
+		t.Errorf("Token length is too short, should be greater than 35 characters")
 	}
 }
