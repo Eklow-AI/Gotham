2ab02bbd1cda1ba84398cce9d92ad5dfa20149ef
Daniel Lobaton <dlobaton@eklow.ai>
2021-05-19 00:52:29 -0400

    some rough concurrency

--- src/sdk/redshirt.go
+++ src/sdk/redshirt.go
@@ -5,7 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"os"
+	"sync"
+	"time"
 )
 
 type searchFilter struct {
@@ -29,8 +30,8 @@ type RSRequest struct {
 	Totals        bool           `json:"totals"`
 	Lists         bool           `json:"lists"`
 	SearchFilters []searchFilter `json:"searchFilter"`
-	Recordperpage int            `json:"recordPerPage"`
-	Currentpage   int            `json:"currentPage"`
+	RecordPerPage int            `json:"recordPerPage"`
+	CurrentPage   int            `json:"currentPage"`
 	SortFilters   []sortFilter   `json:"sortFilter"`
 }
 
@@ -39,9 +40,9 @@ type RSResponse struct {
 	NoOfpages            int          `json:"noofPages"`
 	Currentpage          int          `json:"currentPage"`
 	Recordperpage        int          `json:"recordPerPage"`
-	Awarddiscovered      int          `json:"awardDiscovered"`
-	Totalvalueofawards   int          `json:"totalValueOfAwards"`
-	Averagevalueofawards int          `json:"averageValueOfAwards"`
+	AwardsDiscovered     int          `json:"awardDiscovered"`
+	TotalValueOfAwards   float64      `json:"totalValueOfAwards"`
+	AverageValueOfAwards float64      `json:"averageValueOfAwards"`
 	TimeToExecute        string       `json:"time_to_execute"`
 	ListData             []RSContract `json:"listdata"`
 }
@@ -66,14 +67,33 @@ type RSContract struct {
 	SetAside           *string `json:"type_of_set_aside_description"`
 }
 
+func getRSResponse(query RSRequest) (rsData RSResponse, err error) {
+	data, err := json.Marshal(query)
+	if err != nil {
+		return RSResponse{}, err
+	}
+
+	resp, err := client.Post("https://redshirttest.g2xchange.com/wp-json/api/v1/query/?db=MM10TEST", "application/json", bytes.NewBuffer(data))
+
+	if err != nil {
+		return RSResponse{}, err
+	}
+	err = json.NewDecoder(resp.Body).Decode(&rsData)
+
+	if err != nil {
+		return RSResponse{}, err
+	}
+	return rsData, err
+}
+
 func getContractFromID(id string) RSContract {
 	query := RSRequest{
 		Object:      "contracts",
-		Version:     "1.0",
+		Version:     "1.2",
 		TimeOut:     45000,
 		RecordLimit: 10000,
 		Rows:        true,
-		Totals:      true,
+		Totals:      false,
 		Lists:       false,
 		SearchFilters: []searchFilter{
 			{
@@ -82,8 +102,8 @@ func getContractFromID(id string) RSContract {
 				Value:    id,
 			},
 		},
-		Recordperpage: 1,
-		Currentpage:   1,
+		RecordPerPage: 1,
+		CurrentPage:   1,
 		SortFilters: []sortFilter{
 			{
 				Field: "date_signed",
@@ -91,24 +111,112 @@ func getContractFromID(id string) RSContract {
 			},
 		},
 	}
-	data, err := json.Marshal(query)
-	if err != nil {
-		log.Fatal(err)
-	}
-	resp, err := client.Post(fmt.Sprintf("%s%s", os.Getenv("RS_URI"), "/wp-json/api/v1/query/?db=MM10TEST"), "application/json", bytes.NewBuffer(data))
+
+	data, err := getRSResponse(query)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var rsData RSResponse
-	err = json.NewDecoder(resp.Body).Decode(&rsData)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	contracts := rsData.ListData
+	contracts := data.ListData
 	if len(contracts) < 1 {
 		return RSContract{}
 	}
 	return contracts[0]
+}
+
+func GetContractsFromCage(cage string) []RSContract {
+	start := time.Now()
+	defer func() {
+		fmt.Println("Execution Time: ", time.Since(start))
+	}()
+
+	recordsPerPage := 1
+	currPage := 1
+	query := RSRequest{
+		Object:      "contracts",
+		Version:     "1.2",
+		TimeOut:     45000,
+		RecordLimit: 10000,
+		Rows:        true,
+		Totals:      true,
+		Lists:       false,
+		SearchFilters: []searchFilter{
+			{
+				Field:    "cage_code",
+				Operator: "eq",
+				Value:    cage,
+			},
+		},
+		RecordPerPage: recordsPerPage,
+		CurrentPage:   currPage,
+		SortFilters: []sortFilter{
+			{
+				Field: "date_signed",
+				Order: "desc",
+			},
+		},
+	}
+	data, err := getRSResponse(query)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(data.ListData) < 1 {
+		return []RSContract{}
+	}
+
+	// grab the first contract, create an array the size of all contracts discovered, and then
+	// add the first contract to that array
+	contract := data.ListData[0]
+	base := 10
+	pages := data.AwardsDiscovered / base
+	if data.AwardsDiscovered%base != 0 {
+		pages += 1
+	}
+
+	contracts := make([]RSContract, pages)
+	contracts[0] = contract
+	wg := sync.WaitGroup{}
+	// Get the rest of the contracts concurrently
+	for page := 1; page < pages; page++ {
+		wg.Add(1)
+
+		go func(currPage int) {
+			query = RSRequest{
+				Object:      "contracts",
+				Version:     "1.2",
+				TimeOut:     45000,
+				RecordLimit: 10000,
+				Rows:        true,
+				Totals:      true,
+				Lists:       false,
+				SearchFilters: []searchFilter{
+					{
+						Field:    "cage_code",
+						Operator: "eq",
+						Value:    cage,
+					},
+				},
+				RecordPerPage: base,
+				CurrentPage:   currPage,
+				SortFilters: []sortFilter{
+					{
+						Field: "date_signed",
+						Order: "desc",
+					},
+				},
+			}
+			response, err := getRSResponse(query)
+			contract = response.ListData[0]
+			contracts[currPage-1] = contract
+			if err != nil {
+				log.Fatal(err)
+			}
+			wg.Done()
+		}(page + 1)
+	}
+	wg.Wait()
+	return contracts
 }
